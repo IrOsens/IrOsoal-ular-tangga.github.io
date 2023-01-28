5b1fdc3fa9b7d1bac9d874b2e24be0a2b69f1f5e
IrOsens <108106124+IrOsens@users.noreply.github.com>
2023-01-28 22:02:38 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-let answers = document.querySelectorAll(".answer");
+for (let i = 1; i <= 50; i++) {
+    document.getElementById(`answer-${i}`).addEventListener("click", function() {
+        window.location.href = `soal/soal-${i}.html`;
+    });
+  }
+//let answers = document.querySelectorAll(".answer");
 
-for (let i = 0; i < answers.length; i++) {
+//for (let i = 0; i < answers.length; i++) {
 
-  answers[i].addEventListener("click", function() {
+ //answers[i].addEventListener("click", function() {
 
-    if (this.id === "answer-1") {
+    //if (this.id === "answer-1") {
 
-      this.classList.add("correct");
+      //his.classList.add("correct");
 
-    } else {
+    //} else {
 
-      this.classList.add("incorrect");
+      //this.classList.add("incorrect");
 
-    }
+    //}
 
-  });
+  //});
 
-}
+//}
 
