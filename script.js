b2c50f750c363561837355cf41a811c8105dadb8
IrOsens <108106124+IrOsens@users.noreply.github.com>
2023-01-29 15:00:04 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ for (let i = 1; i <= 50; i++) {
         window.location.href = `soal/soal-${i}.html`;
     });
   }
+var dice = document.getElementById("dice-img");
+var result = document.getElementById("result");
+dice.onclick = function() {
+  var randomNumber = Math.floor(Math.random() * 6) + 1;
+  dice.src = "dadu/dice" + randomNumber + ".png";
+  dice.classList.add("rotate");
+  result.innerHTML = randomNumber;
+  setTimeout(function(){
+   dice.classList.remove("rotate");
+  },500);
+}
+
 //let answers = document.querySelectorAll(".answer");
 
 //for (let i = 0; i < answers.length; i++) {
